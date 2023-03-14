0514e0d12c4c7ad8b7e2cf0645c165a4ac2197a3
Roman Goduhin <superchewits2000@gmail.com>
2023-03-14 14:20:47 +0300

    feat: add media query for Input, create for it separated style file

--- my-app/src/components/Search/Input/Input.tsx
+++ my-app/src/components/Search/Input/Input.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Props} from "./Input.types";
+import styles from "./Input.module.scss";
 
 
 function Input({value, setValue}: Props) {
     return (
         <input value={value}
+               className={styles.customInput}
                onChange={(e) => setValue(e.target.value)}
                placeholder="Type name, username or email"
                type="text"
