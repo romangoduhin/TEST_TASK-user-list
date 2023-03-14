408c937653c2df40482c94bc443a3d40c44f75f9
Roman Goduhin <superchewits2000@gmail.com>
2023-03-14 13:04:00 +0300

    feat: update Info component

--- my-app/src/components/List/Item/Info/Info.tsx
+++ my-app/src/components/List/Item/Info/Info.tsx
@@ -5,22 +5,29 @@ import {Props} from "./Info.types";
 
 
 function Info({searchValue, name, username, email}: Props) {
+    const items = [
+        {
+            label: "Name: ",
+            value: name,
+        },
+        {
+            label: "Username: ",
+            value: username,
+        },
+        {
+            label: "Email: ",
+            value: email,
+        },
+    ]
+
     return (
         <div className={styles.info}>
-            <div>
-                <span>Name:</span>
-                <MarkedText innerValue={name} searchValue={searchValue}/>
-            </div>
-
-            <div>
-                <span>Username:</span>
-                <MarkedText innerValue={username} searchValue={searchValue}/>
-            </div>
-
-            <div>
-                <span>Email:</span>
-                <MarkedText innerValue={email} searchValue={searchValue}/>
-            </div>
+            {items.map(item => (
+                <div>
+                    <span>{item.label}</span>
+                    <MarkedText innerValue={item.value} searchValue={searchValue}/>
+                </div>
+            ))}
         </div>
     );
 }
